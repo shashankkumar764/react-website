5ed2eb3cca99b80e3876429ff912a16bd8246b85
shashank kumar <shashank.kumar764@gmail.com>
2024-03-08 00:13:55 +0530

    added reference for navbar

--- src/navbar/Header.jsx
+++ src/navbar/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 const Header = () => {
+    //take reference from this website, https://testmocks.com/ like it has black horizontally scrollable bar in mobile
   return (
     <div>From Header component. this will display a horizontally scrollable navbar, containing various routes links</div>
   )
--- src/navbar/Navbar.jsx
+++ src/navbar/Navbar.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 
 const Navbar = () => {
+    //take reference of navbar from this website https://cbphysiotherapy.in/
+
   return (
     <div>From Navbar component. This will display nested navbar</div>
   )
